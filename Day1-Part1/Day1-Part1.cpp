0f7ea3850146b0def1d54cf9254d539fd390a312
joeyl83 <124374112+joeyl83@users.noreply.github.com>
2023-10-22 22:35:34 -0400

    Finished code for Day1-Part1

--- Day1-Part1/Day1-Part1.cpp
+++ Day1-Part1/Day1-Part1.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
-    
+    fstream my_file;
+    my_file.open("input2.txt", ios::in);
+
+    vector<int> a;
+    string line;
+
+    while (!my_file.eof())
+    {
+        my_file >> line;
+        a.push_back(stoi(line));
+    }
+    my_file.close();
+
+    for (int i = 0; i < a.size() - 1; i++)
+    {
+        for (int j = i + 1; j < a.size(); j++)
+        {
+            if (a[i] + a[j] == 2020)
+            {
+                cout << a[i] * a[j];
+            }
+        }
+    }
 }
