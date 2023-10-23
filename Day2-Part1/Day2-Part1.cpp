d5e54e4ebe329b4309be410642d5b695751c6747
joeyl83 <124374112+joeyl83@users.noreply.github.com>
2023-10-22 23:36:18 -0400

    Got file input

--- Day2-Part1/Day2-Part1.cpp
+++ Day2-Part1/Day2-Part1.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    fstream file;
+    file.open("input.txt", ios::in);
+
+    vector<string> a;
+    string line;
+
+    while (!file.eof())
+    {
+        file >> line;
+        a.push_back(line);
+    }
+    file.close();
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        int j = 0;
+        stringstream ssin(a[i]);
+        while (ssin.good() && i < 3) 
+        {
+
+        }
+    }
 }
