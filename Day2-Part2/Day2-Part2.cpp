d68ec7503e74116af32a93fc3f6970f6fbd3e1df
joeyl83 <124374112+joeyl83@users.noreply.github.com>
2023-10-24 17:39:40 -0400

    Completed Day2-Part2

---
+++ Day2-Part2/Day2-Part2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    int validCount = 0;
+
+    std::ifstream file("input.txt");
+    string line;
+
+    while (std::getline(file, line))
+    {
+        string range;
+        string letterRaw;
+        string password;
+
+        int lowerRange, upperRange;
+        char dash;
+        stringstream ss(line);
+        ss >> lowerRange >> dash >> upperRange >> letterRaw >> password;
+
+        char letter = letterRaw[0];
+
+        int count = 0;
+        if (password[lowerRange - 1] == letter)
+        {
+            count++;
+        }
+        if (password[upperRange - 1] == letter)
+        {
+            count++;
+        }
+
+        if (count == 1)
+        {
+            validCount++;
+        }
+    }
+    std::cout << validCount;
+    return 0;
+}
